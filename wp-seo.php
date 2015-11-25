5d3af0ba67ad59e9a81ba251b94a7ee91a3c09cb
Piet Bos <info@senlinonline.com>
2015-11-25 18:46:14 +0800

    initial commit v0.1.0 Yoast Minus Bloat Equals SEO
    fork of Yoast SEO v2.3.5 which we will use as starting point to remove
    all the unnecessary bloat.

---
+++ wp-seo.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @package YMBESEO\Main
+ */
+
+/**
+ * Plugin Name: Yoast Minus Bloat Equals SEO
+ * Version: 0.1 (fork from Yoast SEO 2.3.5)
+ * Plugin URI: https://so-wp.com/plugins/ymbeseo
+ * Description: Yoast Minus Bloat Equals SEO
+ * Author: SO WP
+ * Author URI: https://so-wp.com/
+ * Text Domain: ymbeseo
+ * Domain Path: /languages/
+ * License: GPL v3
+ */
+
+/**
+ * Yoast Minus Bloat Equals SEO plugin
+ * Copyright (C) 2015-2016, SO WP - support@so-wp.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+if ( ! function_exists( 'add_filter' ) ) {
+	header( 'Status: 403 Forbidden' );
+	header( 'HTTP/1.1 403 Forbidden' );
+	exit();
+}
+
+if ( ! defined( 'WPSEO_FILE' ) ) {
+	define( 'WPSEO_FILE', __FILE__ );
+}
+
+// Load the Yoast SEO plugin.
+require_once( dirname( __FILE__ ) . '/wp-seo-main.php' );
