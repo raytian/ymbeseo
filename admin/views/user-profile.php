c96c4b5f25f6d5d1664edaa1912c0a038f097e6f
Piet Bos <info@senlinonline.com>
2015-12-14 11:44:46 +0800

    make file layout better readable

--- admin/views/user-profile.php
+++ admin/views/user-profile.php
@@ -12,27 +12,33 @@
 <table class="form-table">
 	<tr>
 		<th>
-			<label
-				for="ymbeseo_author_title"><?php _e( 'Title to use for Author page', 'ymbeseo' ); ?></label>
-		</th>
-		<td><input class="regular-text" type="text" id="ymbeseo_author_title" name="ymbeseo_author_title"
-		           value="<?php echo esc_attr( get_the_author_meta( 'ymbeseo_title', $user->ID ) ); ?>"/>
-		</td>
-	</tr>
-	<tr>
-		<th>
-			<label
-				for="ymbeseo_author_metadesc"><?php _e( 'Meta description to use for Author page', 'ymbeseo' ); ?></label>
+			<label for="ymbeseo_author_title">
+				<?php _e( 'Title to use for Author page', 'ymbeseo' ); ?>
+			</label>
 		</th>
 		<td>
-						<textarea rows="3" cols="30" id="ymbeseo_author_metadesc"
-						          name="ymbeseo_author_metadesc"><?php echo esc_textarea( get_the_author_meta( 'ymbeseo_metadesc', $user->ID ) ); ?></textarea>
+			<input class="regular-text" type="text" id="ymbeseo_author_title" 
+				name="ymbeseo_author_title"
+				value="<?php echo esc_attr( get_the_author_meta( 'ymbeseo_title', $user->ID ) ); ?>" />
 		</td>
 	</tr>
 	<tr>
 		<th>
-			<label
-				for="ymbeseo_author_exclude"><?php _e( 'Exclude user from Author-sitemap', 'ymbeseo' ); ?></label>
+			<label for="ymbeseo_author_metadesc">
+				<?php _e( 'Meta description to use for Author page', 'ymbeseo' ); ?>
+			</label>
+		</th>
+		<td>
+			<textarea rows="3" cols="30" id="ymbeseo_author_metadesc" name="ymbeseo_author_metadesc">
+				<?php echo esc_textarea( get_the_author_meta( 'ymbeseo_metadesc', $user->ID ) ); ?>
+			</textarea>
+		</td>
+	</tr>
+	<tr>
+		<th>
+			<label for="ymbeseo_author_exclude">
+				<?php _e( 'Exclude user from Author-sitemap', 'ymbeseo' ); ?>
+			</label>
 		</th>
 		<td>
 			<input class="checkbox double" type="checkbox" id="ymbeseo_author_exclude"
